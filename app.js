ee491afd4f244d706f39998a19d6e651618d3c3a
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-24 15:46:15 +0100

    attempt import jquery as module

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("connected");
+
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
+
+
+$("p").style.color("red");
+
+modules.export = jquery;
